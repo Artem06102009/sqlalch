a5c2be67ff66bfe2775054a30f75e98bdbf2bc7f
artemkolegov <kolegov_ai@mail.ru>
2026-04-07 17:26:18 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,66 @@
+# from alca.forms.registerform import RegisterForm
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+import datetime
+from data import db_session
+from data.users import User
+
+db_session.global_init("db/blogs.db")
+session = db_session.create_session()
+
+
+def make_user(surname, name, age, position, speciality, address, email):
+    user = User()
+    user.surname = surname
+    user.name = name
+    user.age = age
+    user.position = position
+    user.speciality = speciality
+    user.address = address
+    user.email = email
+    user.modified_date = datetime.datetime.now()
+    session.add(user)
+
+
+make_user(
+    surname="Scott",
+    name="Ridley",
+    age=21,
+    position="captain",
+    speciality="research engineer",
+    address="module_1",
+    email="scott_chief@mars.org"
+)
+
+make_user(
+    surname="Watney",
+    name="Mark",
+    age=25,
+    position="botanist",
+    speciality="agriculture",
+    address="module_2",
+    email="watney@mars.org"
+)
+
+make_user(
+    surname="Vogel",
+    name="Alex",
+    age=30,
+    position="chemist",
+    speciality="chemistry",
+    address="module_2",
+    email="vogel@mars.org"
+)
+
+make_user(
+    surname="Johanssen",
+    name="Beth",
+    age=28,
+    position="programmer",
+    speciality="software",
+    address="module_3",
+    email="johanssen@mars.org"
+)
+
+session.commit()
+session.close()
