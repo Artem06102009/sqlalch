0a2ccf482ff851328e1f020f0b7faa8a7d4cde86
artemkolegov <kolegov_ai@mail.ru>
2026-04-07 17:46:19 +0300

    Первая работа

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import datetime
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 
 db_session.global_init("db/blogs.db")
 session = db_session.create_session()
@@ -61,6 +62,15 @@ make_user(
     address="module_3",
     email="johanssen@mars.org"
 )
+
+job = Jobs()
+job.team_leader = 1
+job.job = "deployment of residential modules 1 and 2"
+job.work_size = 15
+job.collaborators = "2, 3"
+job.start_date = datetime.datetime.now()
+job.is_finished = False
+session.add(job)
 
 session.commit()
 session.close()
